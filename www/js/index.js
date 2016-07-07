1153d166ba4af0a9094a2b0cb7c15897c43de27c
Claudio Cardinale <cardinale.claudio@gmail.com>
2016-07-08 00:05:05 +0200

    draft camera functionalities

--- www/js/index.js
+++ www/js/index.js
@@ -68,6 +68,16 @@ var app = {
                 console.log(e);
                 alert(e);
             });
+/*
+        navigator.camera.getPicture(onSuccess, onFail, {});
+
+        function onSuccess() {
+            console.log("Camera getPicture success.")
+        }
+
+        function onFail(message) {
+            alert('Failed because: ' + message);
+        }*/
        // }
     },
     // Update DOM on a Received Event
@@ -80,5 +90,96 @@ var app = {
         receivedElement.setAttribute('style', 'display:block;');
 
         console.log('Received Event: ' + id);
+    }
+};
+
+var camera = {
+    setOptions: function(srcType) {
+        var options = {
+            // Some common settings are 20, 50, and 100
+            quality: 50,
+            destinationType: Camera.DestinationType.FILE_URI,
+            // In this app, dynamically set the picture source, Camera or photo gallery
+            sourceType: srcType,
+            encodingType: Camera.EncodingType.JPEG,
+            mediaType: Camera.MediaType.PICTURE,
+            allowEdit: true,
+            correctOrientation: true  //Corrects Android orientation quirks
+        }
+        return options;
+    },
+
+    openCamera: function (selection) {
+
+        var srcType = Camera.PictureSourceType.CAMERA;
+        var options = this.setOptions(srcType);
+        var func = this.createNewFileEntry;
+        var _this = this;
+
+        navigator.camera.getPicture(function cameraSuccess(imageUri) {
+
+            _this.displayImage(imageUri);
+            // You may choose to copy the picture, save it somewhere, or upload.
+            //func(imageUri);
+
+        }, function cameraError(error) {
+            console.debug("Unable to obtain picture: " + error, "app");
+
+        }, options);
+    },
+
+    displayImage: function (imgUri) {
+
+        var elem = document.getElementById('imageFile');
+        elem.src = imgUri;
+    },
+
+    openFilePicker: function(selection) {
+
+        var srcType = Camera.PictureSourceType.SAVEDPHOTOALBUM;
+        var options = this.setOptions(srcType);
+        var func = this.createNewFileEntry;
+        var _this = this;
+
+        navigator.camera.getPicture(function cameraSuccess(imageUri) {
+
+            // Do something
+            _this.displayImage(imageUri);
+
+        }, function cameraError(error) {
+            console.debug("Unable to obtain picture: " + error, "app");
+
+        }, options);
+    },
+
+    getFileEntry: function (imgUri) {
+        window.resolveLocalFileSystemURL(imgUri, function success(fileEntry) {
+
+            // Do something with the FileEntry object, like write to it, upload it, etc.
+            // writeFile(fileEntry, imgUri);
+            console.log("got file: " + fileEntry.fullPath);
+            // displayFileData(fileEntry.nativeURL, "Native URL");
+
+        }, function () {
+            // If don't get the FileEntry (which may happen when testing
+            // on some emulators), copy to a new FileEntry.
+            this.createNewFileEntry(imgUri);
+        });
+    },
+
+    createNewFileEntry: function (imgUri) {
+        window.resolveLocalFileSystemURL(cordova.file.cacheDirectory, function success(dirEntry) {
+
+            // JPEG file
+            dirEntry.getFile("tempFile.jpeg", { create: true, exclusive: false }, function (fileEntry) {
+
+                // Do something with it, like write to it, upload it, etc.
+                // writeFile(fileEntry, imgUri);
+                console.log("got file: " + fileEntry.fullPath);
+                // displayFileData(fileEntry.fullPath, "File copied to");
+
+            }, onErrorCreateFile);
+
+        }, onErrorResolveUrl);
     }
 };
